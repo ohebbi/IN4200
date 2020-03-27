5734e99c0aa5d775f7e935ddd7b98eea62cb8f87
Oliver <oliverlh@uio.no>
2020-03-27 12:19:26 +0100

    pragma

--- home_exam1/top_n_webpages.c
+++ home_exam1/top_n_webpages.c
@@ -12,8 +12,8 @@ void top_n_webpages(int num_webpages, int *num_involvements, int n) {
   alloc1D(&top_webpages,n);
 
   #pragma omp parallel for ordered schedule(dynamic)
-  for(int i=1; i < (n); i++){
-      #pragma omp ordered
+  for(int i=0; i < (n); i++){
+
       for (int j=0; j < (num_webpages); j++){
           if (num_involvements[j] > top_webpages[0]){
               top_webpages[0] = num_involvements[j];
@@ -25,7 +25,7 @@ void top_n_webpages(int num_webpages, int *num_involvements, int n) {
               index[i]        = j;
 
               // Check for earlier occurents with this index
-
+              #pragma omp ordered
               for (int k = 0; k < i; k++){
 
                   if (index[k] == j){
