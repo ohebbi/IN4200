fdb3418bc9ca6d6f99c9a99883234dc76dcdc837
Oliver <oliverlh@uio.no>
2020-03-27 10:39:10 +0100

    pragma

--- home_exam1/top_n_webpages.c
+++ home_exam1/top_n_webpages.c
@@ -12,7 +12,7 @@ void top_n_webpages(int num_webpages, int *num_involvements, int n) {
   alloc1D(&top_webpages,n);
 
   for(int i=0; i < (n); i++){
-      #pragma omp parallel for reduction{+:top_webpages, index}
+      #pragma omp parallel for reduction(+:top_webpages, index)
       for (int j=0; j < (num_webpages); j++){
           if (num_involvements[j] > top_webpages[0]){
               top_webpages[0] = num_involvements[j];
