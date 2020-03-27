1d1a355c90ecd9f91bb1822df41be78c7815b27d
Oliver <oliverlh@uio.no>
2020-03-27 10:18:18 +0100

    top_n_webpages

--- home_exam1/top_n_webpages.c
+++ home_exam1/top_n_webpages.c
@@ -2,30 +2,46 @@
 #include <stdio.h>  // printf
 
 #include "functions/functions.h"
-int *array;
-
-int compare_func (const void * a, const void * b) {
-    int ia = *(int *)a;
-    int ib = *(int *)b;
-
-    return array[ia] < array[ib] ? -1 : array[ia] > array[ib];
-}
 
 void top_n_webpages(int num_webpages, int *num_involvements, int n) {
 
   int *index;
-  alloc1D(&index,num_webpages);
+  alloc1D(&index,n);
 
-  for(int i=0;i<(num_webpages);i++){
-      index[i]=i;
+  int *top_webpages;
+  alloc1D(&top_webpages,n);
+
+  #pragma omp parallel for reduction(+:top_webpages, index)
+  for(int i=0; i < (n); i++){
+      for (int j=0; j < (num_webpages); j++){
+          if (num_involvements[j] > top_webpages[0]){
+              top_webpages[0] = num_involvements[j];
+              index[0]        = j;
+          }
+
+          //
+          else if ((num_involvements[j] > top_webpages[i]) && (num_involvements[j] <= top_webpages[i-1])){
+              top_webpages[i] = num_involvements[j];
+              index[i]        = j;
+
+              // Check for earlier occurents with this index
+              for (int k = 0; k < i; k++){
+                  if (index[k] == j){
+                      top_webpages[i] = 0;
+                      index[i]        = 0;
+                  }
+              }
+          }
+      }
   }
-  array = num_involvements;
-  qsort(index, num_webpages, sizeof(*index), compare_func);
+
   printf("\ndata\tindex\n");
   printf("===============\n");
-  #pragma omp parallel for
-  for (int i = num_webpages - 1; i > num_webpages - 1 - n; i--){
-      printf("%d\t%d\n", num_involvements[index[i]], index[i]);
+  for (int i = 0; i < n; i++){
+      printf("%d\t%d\n", top_webpages[i], index[i]);
   }
+
   printf("===============\n");
+  free(index);
+  free(top_webpages);
 }
